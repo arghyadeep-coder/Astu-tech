d6fd656dce4a315e03831261f33b28b585b2e2e6
arghyadeep_coder <arghyadeepbankura@gmail.com>
2023-04-30 20:40:39 +0530

    Debugging

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,14 +1,14 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from "react-router-dom";
 import NotFound from "pages/NotFound";
 const Invoice = React.lazy(() => import("pages/Invoice"));
 const ProjectRoutes = () => {
   return (
     <React.Suspense fallback={<>Loading...</>}>
+      <BrowserRouter   basename="/Astu-tech" />
       <Router>
         <Routes>
-          <BrowserRouter   basename="/Astu-tech/" />
-          <Route path="/" element={<Invoice />} />
+          <Route path="/Astu-tech" element={<Invoice />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
       </Router>
